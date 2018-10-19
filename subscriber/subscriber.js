7931f898e193e4ec17434eaccfa2050a5eb56133
kevin Seidel <kevin.seidel@karrieretutor.de>
2018-10-19 16:04:18 +0200

    trying to list subs, does not work 403

---
+++ subscriber/subscriber.js
@@ -0,0 +1,30 @@
+const PubSub = require('@google-cloud/pubsub');
+
+var project_id  = process.env.PROJECT_ID;
+var keyfile     = process.env.KEYFILE;
+
+//check if variables are defined/assign default values
+if ( typeof project_id === 'undefined' ) {
+  console.log("ERR: no project_id");
+  process.exit(1);
+}
+
+if ( typeof keyfile === 'undefined' ) {
+  keyfile = "/etc/keyfile.json"
+}
+
+const pubsub = new PubSub({
+  projectID: project_id,
+  keyFilename: keyfile
+});
+
+console.log(pubsub);
+
+pubsub.getSubscriptions( (err, subscriptions) => {
+  if (!err)  {
+    console.log(subscriptions);
+  }
+  else {
+    console.log(err);
+  }
+});
