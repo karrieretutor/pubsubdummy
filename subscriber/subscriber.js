27da5764a841c7fedffc248fb310a1119a8ab71c
kevin Seidel <kevin.seidel@karrieretutor.de>
2018-10-22 15:20:33 +0200

    added message worker which simulates load during work on msg, msg backlog

--- subscriber/subscriber.js
+++ subscriber/subscriber.js
@@ -1,16 +1,19 @@
 const PubSub = require('@google-cloud/pubsub');
 
-var project_id  = process.env.PROJECT_ID;
-var keyfile     = process.env.KEYFILE;
+var project_id        = process.env.PROJECT_ID;
+var keyfile           = process.env.KEYFILE || "/etc/keyfile.json";
+var subscriptionname  = process.env.SUBSCRIPTION;
+var maxBacklog        = process.env.MAXBACKLOG || 2;
 
-//check if variables are defined/assign default values
+/*
+ *  check variables
+ */
 if ( typeof project_id === 'undefined' ) {
-  console.log("ERR: no project_id");
-  process.exit(1);
+  throw ( new Error("no project_id defined") );
 }
 
-if ( typeof keyfile === 'undefined' ) {
-  keyfile = "/etc/keyfile.json"
+if ( typeof subscriptionname === 'undefined' ) {
+  throw ( new Error("no subscription name defined") );
 }
 
 const pubsub = new PubSub({
@@ -18,13 +21,39 @@ const pubsub = new PubSub({
   keyFilename: keyfile
 });
 
-pubsub.getSubscriptions( (err, subscriptions) => {
-  if (!err)  {
-    subscriptions.forEach ( (subscription) => {
-      console.log(subscription.name);
-    });
+/*
+ *  get the subscription and read from it 
+ *  periodically, which simulates some load
+ */
+
+var subscription    = pubsub.subscription( subscriptionname );
+
+//stores messages we are currently working on
+var messageBacklog  = [];
+
+var messageWorker = function ( message ) {
+  //just an example
+  console.log( "worked on message " + message.id );
+  console.log( message.data );
+
+  //remove message from backglog
+  let i = messageBacklog.indexOf( message );
+  messageBacklog.splice( i, 1 );
+}
+
+var onMessage = function ( message ) {
+  //if backlog is full, return
+  if ( messageBacklog.length >= maxBacklog ) {
+    console.log( "cannot work on message " + message.id + ", backlog full");
+    return;
   }
-  else {
-    console.log(err);
-  }
-});
+  
+  //first ack message to avoid multiple instances working on it
+  message.ack();
+  messageBacklog.push( message );
+
+  //the delay is just for simulating the worker to actually take some time
+  setTimeout( messageWorker, 5000, message );
+}
+
+subscription.on( "message", onMessage );
